9b067d8118d4eee45046997e758f9685ffd709f1
masereje-faith <fmasereje@gmail.com>
2025-12-04 14:28:09 +0300

    my new branch

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const name = "masereje faith";
+const age = 18;
+const favouritefood = Pizza;
+const nationality = Ugandan;
+const student = yes;
+const dreamsalary =1000000
+
+console.log("-----my profile----")
+console.log("name",name)
+console.log("age",age)
+console.log("favouritefood",pizza)
+console.log("nationality",nationality)
+console.log("student",student)
+console.log("dreamsalary",1000000)
+console.log("------end of profile---")
+
