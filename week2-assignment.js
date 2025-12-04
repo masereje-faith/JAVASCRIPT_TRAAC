2ff7dff1d8e9d005d196a0ddc6e1d1c7557f393f
masereje-faith <fmasereje@gmail.com>
2025-12-04 17:31:36 +0300

    week2-assignment

---
+++ week2-assignment.js
@@ -0,0 +1,15 @@
+const name = "masereje faith";
+const age = 18;
+const whetherilikecoding = true;
+const myfavouritecolor = "purple";
+const district = "kampala";
+const numberofsiblings = 4;
+
+console.log("-----my profile----")
+console.log("name",name)
+console.log("age",age)
+console.log("whetherilikecoding",whetherilikecoding)
+console.log("myfavouritecolor",myfavouritecolor)
+console.log("district",district)
+console.log("numberofsiblings",numberofsiblings)
+console.log("------end of profile---")
