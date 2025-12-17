17feaff7de99d5e76dd1c82affaa89777d8f9b5b
masereje-faith <fmasereje@gmail.com>
2025-12-17 22:43:20 +0300

    operators.js

---
+++ operators.js
@@ -0,0 +1,80 @@
+ /* // A) arithemetic operators
+ let a = 10;
+ let b = 5;
+ let c = 6;
+ let d = 21;
+
+ let sum = a + b;           // additon
+
+ console.log(a + b) // addition
+ console.log(a - b) // subtraction
+ console.log(a * b) // multiplication
+ console.log(a / b) // division
+ console.log (a % b) // modulus 
+  */
+
+
+ /*let a = "Faith"; // string type
+ let b = "Saint"; // string type
+ let c = 5; // number type
+ let d = "10"; // string type
+ let e = 3; // number type
+ let f = "3"; // string type
+
+ console.log(a + c) // string with concatenation with number
+ console.log(a - d) // string with concatenation with number
+ console.log(a * d) // number * string
+ console.log(e + f) // number + string
+ console.log(b - d) // string with concatenation with number
+ console.log(d + f) // string + number
+ console.log(d + c) // string + number
+ console.log(d / c) // string / number
+ */
+
+  /*// B) Assignment operators (Shorthand operators)
+
+  let x = 10;
+ x = 20; // normal assignment
+ x += 10; // x = x + 10
+ x -= 15; // x = x - 15
+ x *= 2; // x = x * 2
+ x /= 6; // x = x / 6
+
+ console.log(x)
+ // TRIAL
+  let y = 50; 
+  y = 100; // normal assignment
+  y += 20; // y = y + 100
+  y -= 10; // y = y - 10
+  y *= 5;// y = y * 5
+  y/= 2; // y = y / 2
+
+  console.log(y)
+  */
+ /*// C) Comparison operators
+  let x = 5;
+  let y = 2;
+
+  console.log(x > y);
+  console.log(x < y);
+  console.log(x >= y);
+  console.log(x <= y);
+  */
+ let x = 3 // number type
+ let y ="3" // string type
+ console.log(x == y) // loose comparison(numbers only)
+ console.log(x === y) // strict comparison (values and types)
+
+
+
+
+
+
+
+ 
+
+
+
+
+ 
+ 
