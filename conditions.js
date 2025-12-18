3a4656cd8dc0608eb895a699779a7cdaee947edd
masereje-faith <fmasereje@gmail.com>
2025-12-18 11:32:22 +0300

    conditions.js

---
+++ conditions.js
@@ -0,0 +1,67 @@
+ // if statement
+let age = 17;
+
+ if (age >= 15) {
+      console.log("You are eligible to vote")
+ }
+ 
+
+ // if-else else-if statement
+ let score = 70
+
+ if (score >= 90){
+    console.log("Excellent")
+ }else if (score >= 65){
+          console.log("Good")
+ } else {
+         console.log("Needs improvement")
+ }       
+ 
+
+ let username = "Faith"
+ let password  = "masereje8"
+ 
+ if (password === "masereje8") {
+ } else {
+       console.log("Access")
+ }
+
+  let balance =7000;
+  let withdraw = 30000;
+
+  if ( balance <= withdraw){
+       console.log("Insufficient funds")
+  } else {
+        console.log("Withdraw successful")
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
